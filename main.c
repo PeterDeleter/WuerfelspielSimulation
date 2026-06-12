3261cbd2770214bec0114308bd70f4b90e8cffbd
PeterDeleter <heiko.villing00@gmail.com>
2026-06-12 17:22:32 +0200

    Struct Array Keys

--- main.c
+++ main.c
@@ -8,226 +8,23 @@
 
 uint16_t transitions[1296][4];
 
-uint8_t trans_normalized_sorted[56][5];
-uint8_t trans_normalized[155][5];
-uint8_t trans[460][5];
+uint8_t trans_normalized_sorted[1296][5];
+uint8_t trans_normalized[1296][5];
+uint8_t trans[1296][5];
 uint16_t n_tns = 0;
 uint16_t n_tn = 0;
+uint16_t n_ts = 0;
 uint16_t n_t = 0;
 int depth;
 
-// init state map
-
-KHASH_MAP_INIT_INT64(state_map, uint64_t)
-
-khash_t(state_map) *current;
-khash_t(state_map) *next;
-
-// helper functions
-
-static inline uint64_t pack_state(uint16_t s0, uint16_t s1, uint16_t s2, uint16_t s3){
-    return (uint64_t)s0 << 48 | (uint64_t)s1 << 32 | (uint64_t)s2 << 16 | (uint64_t)s3;
-}
-
-static inline uint64_t normalize(uint64_t state){
-    uint16_t s0 = state >> 48;
-    uint16_t s1 = state >> 32;
-    uint16_t s2 = state >> 16;
-    uint16_t s3 = state;
-    uint16_t min = (s0 < s1) ? s0 : s1;
-    uint16_t min2 = (s2 < s3) ? s2 : s3;
+void static inline normalize(uint16_t *vector){
+    uint16_t min = (vector[0] < vector[1]) ? vector[0] : vector[1];
+    uint16_t min2 = (vector[2] < vector[3]) ? vector[2] : vector[3];
     min = (min < min2) ? min : min2;
-    s0 -= min;
-    s1 -= min;
-    s2 -= min;
-    s3 -= min;
-    return pack_state(s0, s1, s2, s3);
-}
-
-static inline void normalize_trans(uint16_t *trans){
-    uint16_t min = (trans[0] < trans[1]) ? trans[0] : trans[1];
-    uint16_t min2 = (trans[2] < trans[3]) ? trans[2] : trans[3];
-    min = (min < min2) ? min : min2;
-    trans[0] -= min;
-    trans[1] -= min;
-    trans[2] -= min;
-    trans[3] -= min;
-}
-
-static inline uint64_t sort4(uint64_t state){          // sorts 4 element states, MUST be normalized
-    uint16_t sorted[3] = {0, 0, 0};
-    uint16_t buffer;
-    uint8_t n_zeros = 0;
-    for (uint8_t i = 0; i < 4; i++){
-        buffer = state >> (16 * i);
-        //printf("%i", buffer);
-        if (buffer != 0){
-            sorted[2 - i + n_zeros] = buffer;
-        }
-        else{
-            n_zeros++;
-        }
-    }
-    //printf("\n[%i, %i, %i, %i] -> ", 0, sorted[0], sorted[1], sorted[2]);
-    if (n_zeros == 1){                                  // sorted[1:3] nonzero and unsorted
-        //printf("n_zeros = %i -> ", n_zeros);
-        if (sorted[1] < sorted[0]){
-            buffer = sorted[0];
-            sorted[0] = sorted[1];
-            sorted[1] = buffer;
-        }
-        if (sorted[2] < sorted[0]){
-            buffer = sorted[0];
-            sorted[0] = sorted[2];
-            sorted[2] = sorted[1];
-            sorted[1] = buffer;
-        }
-        if (sorted[2] < sorted[1]){
-            buffer = sorted[1];
-            sorted[1] = sorted[2];
-            sorted[2] = buffer;
-        }
-    }
-    else if (n_zeros == 2){                                // sorted[2:3] nonzero and unsorted
-        //printf("n_zeros = %i -> ", n_zeros);
-        if (sorted[2] < sorted[1]){
-            buffer = sorted[1];
-            sorted[1] = sorted[2];
-            sorted[2] = buffer;
-        }
-    }
-    else{
-        //printf("n_zeros = %i -> ", n_zeros);
-    }
-    //printf("[%i, %i, %i, %i]\n", 0, sorted[0], sorted[1], sorted[2]);
-    return pack_state(0, sorted[0], sorted[1], sorted[2]);
-}
-
-static inline int is_won(uint64_t state){
-    uint16_t s0 = state >> 48;
-    uint16_t s1 = state >> 32;
-    uint16_t s2 = state >> 16;
-    uint16_t s3 = state;
-    return s2 == 0 || (s1 == s3);
-}
-
-static inline void shift(khash_t(state_map) *state, uint64_t n_games){
-    while ((n_games >> 53) > 0){
-        n_games >>= 1;
-        for (khiter_t k = kh_begin(state);
-            k != kh_end(state);
-            ++k)
-        {
-            if (!kh_exist(state, k))
-                continue;
-
-            kh_value(state, k) >>= 1;
-        }
-    }
-}
-
-void step(khash_t(state_map) *current, khash_t(state_map) *next, int round){
-    uint64_t n_wins = 0;
-    uint64_t n_games = 0;
-    double pct;
-    for (khiter_t k = kh_begin(current);
-        k != kh_end(current);
-        ++k)
-    {
-        if (!kh_exist(current, k))
-            continue;
-
-        uint64_t state_current = kh_key(current, k);
-        uint64_t multiplicity = kh_value(current, k);
-        uint64_t state_next;
-        // state current values
-        uint16_t sc_0 = state_current >> 48;
-        uint16_t sc_1 = state_current >> 32;
-        uint16_t sc_2 = state_current >> 16;
-        uint16_t sc_3 = state_current;
-        // state next values
-        uint16_t sn_0;
-        uint16_t sn_1;
-        uint16_t sn_2;
-        uint16_t sn_3;
-
-        for (int i = 0; i < n_tns; i++){               // saves sorting and normalizing
-            sn_0 = sc_0 + trans_normalized_sorted[i][0];
-            sn_1 = sc_1 + trans_normalized_sorted[i][1];
-            sn_2 = sc_2 + trans_normalized_sorted[i][2];
-            sn_3 = sc_3 + trans_normalized_sorted[i][3];
-
-            state_next = pack_state(sn_0, sn_1, sn_2, sn_3);
-
-            if (is_won(state_next)){
-                n_wins += multiplicity * trans_normalized_sorted[i][4];
-            }
-            else{
-                int ret;
-                khiter_t key_next = kh_put(state_map, next, state_next, &ret);
-                if (ret > 0){
-                    kh_value(next, key_next) = multiplicity * trans_normalized_sorted[i][4];
-                }
-                else{
-                    kh_value(next, key_next) += multiplicity * trans_normalized_sorted[i][4];
-                }
-            }
-            n_games += multiplicity * trans_normalized_sorted[i][4];
-            //printf("[%i, %i, %i, %i]\n", sn_0, sn_1, sn_2, sn_3);
-        }
-        for (int i = 0; i < n_tn; i++){               // saves normalizing
-            sn_0 = sc_0 + trans_normalized[i][0];
-            sn_1 = sc_1 + trans_normalized[i][1];
-            sn_2 = sc_2 + trans_normalized[i][2];
-            sn_3 = sc_3 + trans_normalized[i][3];
-
-            state_next = pack_state(sn_0, sn_1, sn_2, sn_3);
-            state_next = sort4(state_next);
-
-            if (is_won(state_next)){
-                n_wins += multiplicity * trans_normalized[i][4];
-            }
-            else{
-                int ret;
-                khiter_t key_next = kh_put(state_map, next, state_next, &ret);
-                if (ret > 0){
-                    kh_value(next, key_next) = multiplicity * trans_normalized[i][4];
-                }
-                else{
-                    kh_value(next, key_next) += multiplicity * trans_normalized[i][4];
-                }
-            }
-            n_games += multiplicity * trans_normalized[i][4];
-        }
-        for (int i = 0; i < n_t; i++){               // saves nothing lol
-            sn_0 = sc_0 + trans[i][0];
-            sn_1 = sc_1 + trans[i][1];
-            sn_2 = sc_2 + trans[i][2];
-            sn_3 = sc_3 + trans[i][3];
-
-            state_next = pack_state(sn_0, sn_1, sn_2, sn_3);
-            state_next = normalize(state_next);
-            state_next = sort4(state_next);
-
-            if (is_won(state_next)){
-                n_wins += multiplicity * trans[i][4];
-            }
-            else{
-                int ret;
-                khiter_t key_next = kh_put(state_map, next, state_next, &ret);
-                if (ret > 0){
-                    kh_value(next, key_next) = multiplicity * trans[i][4];
-                }
-                else{
-                    kh_value(next, key_next) += multiplicity * trans[i][4];
-                }
-            }
-            n_games += multiplicity * trans[i][4];
-        }
-    }
-    shift(next, n_games);
-    pct = ((float)n_wins / n_games) * 100;
-    printf("Round: %i: %3f%% n_states: %u", round + 1, pct, kh_size(next));
+    vector[0] -= min;
+    vector[1] -= min;
+    vector[2] -= min;
+    vector[3] -= min;
 }
 
 void initialize_transitions(){
@@ -241,7 +38,7 @@ void initialize_transitions(){
                     transitions[m][2] = k;
                     transitions[m][3] = l;
                     //printf("[%i, %i, %i, %i] ", transitions[m][0], transitions[m][1], transitions[m][2], transitions[m][3]);
-                    normalize_trans(transitions[m]);
+                    normalize(transitions[m]);
                     //printf("[%i, %i, %i, %i]\n", transitions[m][0], transitions[m][1], transitions[m][2], transitions[m][3]);
                     m += 1;
                 }
@@ -311,18 +108,190 @@ void initialize_transitions(){
         }
     }
     //printf("n_tns: %i\nn_tn: %i\nn_t: %i\n", n_tns, n_tn, n_t);
-    /*printf("trans_normalized_sorted\n");
-    for (int i = 0; i < n_tns; i++){
-        printf("[%i, %i, %i, %i] mult: %i\n", trans_normalized_sorted[n_tns][0], trans_normalized_sorted[i][1], trans_normalized_sorted[i][2], trans_normalized_sorted[i][3], trans_normalized_sorted[i][4]);
+}
+
+// init state map
+
+static inline int state_equal(State a, State b)
+{
+    return memcmp(&a, &b, sizeof(State)) == 0;
+}
+
+static inline khint_t state_hash(State s)
+{
+    khint_t h = 1469598103934665603ULL;
+
+    for (int i = 0; i < 4; i++) {
+        h ^= s.state[i];
+        h *= 1099511628211ULL;
     }
-    printf("trans_normalized\n");
-    for (int i = 0; i < n_tn; i++){
-        printf("[%i, %i, %i, %i] mult: %i\n", trans_normalized[i][0], trans_normalized[i][1], trans_normalized[i][2], trans_normalized[i][3], trans_normalized[i][4]);
+
+    return h;
+}
+
+KHASH_INIT(
+    state_map,      // name
+    State,          // key type
+    uint64_t,       // value type
+    1,              // map, not set
+    state_hash,
+    state_equal
+)
+
+khash_t(state_map) *current;
+khash_t(state_map) *next;
+
+// helper functions
+
+
+
+void static inline sort4(uint16_t *vector){                       // sorts 4 element vectors, MUST be normalized
+    uint16_t sorted[4] = {0, 0, 0, 0};
+    uint16_t buffer;
+    uint8_t k = 3;
+    for (uint8_t i = 0; i < 4; i++){
+        if (vector[i] != 0){
+            sorted[k] = vector[i];
+            k--;
+        }
     }
-    printf("trans\n");
-    for (int i = 0; i < n_t; i++){
-        printf("[%i, %i, %i, %i] mult: %i\n", trans[i][0], trans[i][1], trans[i][2], trans[i][3], trans[i][4]);
-    }*/
+    if (k == 0){                                    // sorted[1:3] nonzero and unsorted
+        if (sorted[2] < sorted[1]){
+            buffer = sorted[1];
+            sorted[1] = sorted[2];
+            sorted[2] = buffer;
+        }
+        if (sorted[3] < sorted[1]){
+            buffer = sorted[1];
+            sorted[1] = sorted[3];
+            sorted[3] = sorted[2];
+            sorted[2] = buffer;
+        }
+        if (sorted[3] < sorted[2]){
+            buffer = sorted[2];
+            sorted[2] = sorted[3];
+            sorted[3] = buffer;
+        }
+    }
+    else if (k == 1){                                // sorted[2:3] nonzero and unsorted
+        if (sorted[3] < sorted[2]){
+            buffer = sorted[2];
+            sorted[2] = sorted[3];
+            sorted[3] = buffer;
+        }
+    }
+    vector[0] = sorted[0];
+    vector[1] = sorted[1];
+    vector[2] = sorted[2];
+    vector[3] = sorted[3];
+}
+
+int static inline is_won(uint16_t *vector){
+    if (vector[2] == 0 || vector[1] == vector[3]){
+        return 1;
+    }
+    return 0;
+}
+
+void static inline shift(khash_t(state_map) *state, uint64_t n_games){
+    while ((n_games >> 53) > 0){
+        n_games >>= 1;
+        for (khiter_t k = kh_begin(state);
+            k != kh_end(state);
+            ++k)
+        {
+            if (!kh_exist(state, k))
+                continue;
+
+            kh_value(state, k) >>= 1;
+        }
+    }
+}
+
+void step(khash_t(state_map) *current, khash_t(state_map) *next, uint16_t transitions[][4], int round){
+    uint64_t n_wins = 0;
+    uint64_t n_games = 0;
+    double pct;
+    for (khiter_t k = kh_begin(current);
+        k != kh_end(current);
+        ++k)
+    {
+        if (!kh_exist(current, k))
+            continue;
+
+        State state_current = kh_key(current, k);
+        uint64_t multiplicity = kh_value(current, k);
+        State state_next;
+        for (int i = 0; i < n_tns; i++){               // saves sorting and normalizing
+            state_next.state[0] = state_current.state[0] + trans_normalized_sorted[i][0];
+            state_next.state[1] = state_current.state[1] + trans_normalized_sorted[i][1];
+            state_next.state[2] = state_current.state[2] + trans_normalized_sorted[i][2];
+            state_next.state[3] = state_current.state[3] + trans_normalized_sorted[i][3];
+
+            if (is_won(state_next.state)){
+                n_wins += multiplicity * trans_normalized_sorted[i][4];
+            }
+            else{
+                int ret;
+                khiter_t key_next = kh_put(state_map, next, state_next, &ret);
+                if (ret > 0){
+                    kh_value(next, key_next) = multiplicity * trans_normalized_sorted[i][4];
+                }
+                else{
+                    kh_value(next, key_next) += multiplicity * trans_normalized_sorted[i][4];
+                }
+            }
+            n_games += multiplicity * trans_normalized_sorted[i][4];
+        }
+        for (int i = 0; i < n_tn; i++){               // saves normalizing
+            state_next.state[0] = state_current.state[0] + trans_normalized[i][0];
+            state_next.state[1] = state_current.state[1] + trans_normalized[i][1];
+            state_next.state[2] = state_current.state[2] + trans_normalized[i][2];
+            state_next.state[3] = state_current.state[3] + trans_normalized[i][3];
+            sort4(state_next.state);
+
+            if (is_won(state_next.state)){
+                n_wins += multiplicity * trans_normalized[i][4];
+            }
+            else{
+                int ret;
+                khiter_t key_next = kh_put(state_map, next, state_next, &ret);
+                if (ret > 0){
+                    kh_value(next, key_next) = multiplicity * trans_normalized[i][4];
+                }
+                else{
+                    kh_value(next, key_next) += multiplicity * trans_normalized[i][4];
+                }
+            }
+            n_games += multiplicity * trans_normalized[i][4];
+        }
+        for (int i = 0; i < n_t; i++){               // saves nothing lol
+            state_next.state[0] = state_current.state[0] + trans[i][0];
+            state_next.state[1] = state_current.state[1] + trans[i][1];
+            state_next.state[2] = state_current.state[2] + trans[i][2];
+            state_next.state[3] = state_current.state[3] + trans[i][3];
+            normalize(state_next.state);
+            sort4(state_next.state);
+
+            if (is_won(state_next.state)){
+                n_wins += multiplicity * trans[i][4];
+            }
+            else{
+                int ret;
+                khiter_t key_next = kh_put(state_map, next, state_next, &ret);
+                if (ret > 0){
+                    kh_value(next, key_next) = multiplicity * trans[i][4];
+                }
+                else{
+                    kh_value(next, key_next) += multiplicity * trans[i][4];
+                }
+            }
+            n_games += multiplicity * trans[i][4];
+        }
+    }
+    shift(next, n_games);
+    pct = ((float)n_wins / n_games) * 100;
+    printf("Round: %i: %3f n_states: %u", round + 1, pct, kh_size(next));
 }
 
 int main()
@@ -334,7 +303,9 @@ int main()
 
     int ret;
 
-    khiter_t k = kh_put(state_map, current, 0, &ret);
+    State initial_state = {.state = {0, 0, 0, 0}};
+
+    khiter_t k = kh_put(state_map, current, initial_state, &ret);
 
     kh_value(current, k) = 1;
 
@@ -342,12 +313,11 @@ int main()
 
     printf("Depth: ");
     scanf("%i", &depth);
-    printf("\n");
     for (int i = 0; i < depth; i++){
         clock_t start = clock();
-        step(current, next, i);
+        step(current, next, transitions, i);
         clock_t stop = clock();
-        printf(" round time: %.3fs\n", (double)(stop - start) / CLOCKS_PER_SEC);
+        printf(" round time: %.3f\n", (double)(stop - start) / CLOCKS_PER_SEC);
         khash_t(state_map) *tmp = current;
         current = next;
         next = tmp;
